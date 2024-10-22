fa1ef4e8f34b226d13ae905031e89e000aaca202
yasmira17 <yasmiraportillo17@gmail.com>
2024-10-22 13:10:29 -0500

    mi primer commit

---
+++ Funciones.js
@@ -0,0 +1,53 @@
+console.log ("Calcular el Área de un Triangulo")
+let b = 4
+let h = 3
+let resT = ((b*h)/2)
+console.log (("El Área del Triangulo es: ") + (resT))
+console.log ("___________________________________________")
+console.log("Área de un cuadrado")
+let lc = 5
+let resC = lc * lc
+console.log (("El Área del Cuadrado es: ") + (resC))
+console.log ("___________________________________________")
+console.log ("Calcular el Área de un Rectangulo")
+let br = 8
+let hr = 6
+let resR = br*hr
+console.log (("El Área del Rectangulo es: ") + (resR))
+console.log ("___________________________________________")
+console.log ("Calcular el Área de un Rombo")
+let Dma = 16
+let Dme = 12
+let resRo = ((Dma*Dme)/2)
+console.log (("El Área del Rombo es: ") + (resRo))
+console.log ("___________________________________________")
+console.log ("Calcular el Área de un Circulo")
+let ra = 12
+let resCi = 3.1416*(ra**2)
+console.log (("El Área del Circulo es: ") + (resCi))
+console.log ("___________________________________________")
+console.log ("___________________________________________")
+function AT (b,h) {
+    return ResT = ((b*h)/2)
+    }
+    console.log(AT(4,3))
+console.log ("___________________________________________")
+function AC (lc) {
+    return ResC = (lc*lc)
+    }
+    console.log(AC(5,5))
+console.log ("___________________________________________")
+function AR (br,hr) {
+    return ResR = (br*hr)
+    }
+    console.log(AR(8,6))
+console.log ("___________________________________________")
+function ARo (dma,dme) {
+    return ResRo = ((dma*dme)/2)
+    }
+    console.log(ARo(16,12))
+console.log ("___________________________________________")
+function ACi (ra) {
+    return ResCi = 3.1416*(ra**2)
+    }
+    console.log(ACi(12))
---
+++ Práctica1.js
@@ -0,0 +1,4 @@
+//mi primer comentario 
+
+console.log("Hola"+" "+"mundo") //nos muestra el resultado por consola
+console.log("Bienvenido"+" "+"al"+" "+"Sistema")
